b83f599d4769ea1018036fa3e9c1ec1cc7bbd6e0
Mahadi Hasan <12mahadihasan@gmail.com>
2025-06-25 12:24:10 +0600

    chor: removed unused codes

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -17,7 +17,7 @@ import { MsPackageModule } from 'src/ms-package/msPackage.module';
     MsPackageModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '1d' },
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
     }),
   ],
   providers: [
