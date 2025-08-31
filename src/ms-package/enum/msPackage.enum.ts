c6553cdecd2f633170fc2916f2c3156374535979
Mahadi Hasan <12mahadihasan@gmail.com>
2025-08-31 22:20:11 +0600

    fix(membership-purchase): refactored logic to purhcase membership package

--- src/ms-package/enum/msPackage.enum.ts
+++ src/ms-package/enum/msPackage.enum.ts
@@ -1,7 +1,7 @@
 export enum CategoryInfoType {
-  MONTHLY = 'monthly',
-  YEARLY = 'yearly',
-  LIFETIME = 'life_time',
+  MONTHLY_PREMIUM = 'monthly_premium',
+  LIFETIME_PREMIUM = 'lifetime_premium',
+  LIFETIME_FREE = 'lifetime_free',
 }
 
 export enum PackageStatus {
